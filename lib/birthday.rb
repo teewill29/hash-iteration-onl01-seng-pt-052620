48a15c9555b1b902e163c820729590a9cf2692c9
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 23:20:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,7 @@
   end
 # }
 
-def happy_birthday(birthday_kids)
- def age_appropriate_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
 
 
 
@@ -15,7 +14,7 @@ def happy_birthday(birthday_kids)
    else 
      puts "You are too old for this."
 end 
-
+end 
 birthday_kids
 end 
 
