82e8a3cdcfd68c31fed295ee7312960a48cfaee0
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 23:18:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,7 +1,7 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
+#   birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 # }
 
 def happy_birthday(birthday_kids)
