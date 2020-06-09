1d417828bc7789e26153bbad3d4ce0affea99441
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 23:28:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,6 +5,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids = birthday_kids < 12
+  if birthday_kids = birthday_kids < 12
   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end
 end 
